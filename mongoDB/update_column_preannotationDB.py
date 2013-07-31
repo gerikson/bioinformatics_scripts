97d17c59efdb48ad66b88ced6514924bab0aa033
gerikson <gerikson@scripps.edu>
2013-07-31 15:00:24 -0700

    Corect version of update_column_preannotationDB

--- mongoDB/update_column_preannotationDB.py
+++ mongoDB/update_column_preannotationDB.py
@@ -29,7 +29,7 @@ skiped_var = 0
 non_ascii_var = 0
 invar = open(varfilename)
 
-header = ('Gene',	'Gene_Type',	'Location',	'Distance',	'Coding_Impact', 'Protein_Pos',	'Original_AA',	'Allele_AA',	'Start~Stop_Dist',	'Prop_Cons_Affected_Upstream',	'Prop_Cons_Affected_Downstream',	'Trunc_Prediction',	'Conserved46way',	'Conserved46wayPlacental',	'Conserved46wayPrimates',	'ASW_minallele',	'CEU_minallele	CHB_minallele',	'CHD_minallele',	'GIH_minallele',	'JPT_minallele',	'LWK_minallele',	'MEX_minallele',	'MKK_minallele',	'TSI_minallele',	'YRI_minallele',	'1000GENOMES_AF',	'WELLDERLY_AF325',	'NHLBI',	'eQTL_genes',	'miRNA_BS_influenced',	'miRNA_BS_impact',	'miRNA_BS_direct',	'miRNA_BS_deltaG',	'miRNA_genomic',	'miRNA_folding_deltaG',	'miRNA_binding_deltaG',	'miRNA_top_targets_changed',	'Splice_Site_Pred',	'Splicing_Prediction(MaxENT)',	'ESE_sites',	'ESS_sites',	'Protein_Impact_Prediction(Polyphen)',	'Protein_Impact_Probability(Polyphen)',	'Protein_Impact_Prediction(SIFT)',	'Protein_Impact_Score(SIFT)',	'Protein_Domains',	'Protein_Domains_Impact(LogRE)',	'Protein_Impact_Prediction(Condel)',	'Protein_Impact_Probability(Condel)',	'TF_Binding_Sites',	'TFBS_deltaS',	'omimGene_ID~omimGene_association',	'Protein_Domain_Gene_Ontology',	'dbSNP_ID',	'HGMD_Variant~PubMedID',	'HGMD_Gene~disease_association',	'Genetic_Association_Database~PubMedID',	'PharmGKB_Database~Drug',	'Inheritance~Penetrance',	'Severity~Treatability',	'COSMIC_Variant~NumSamples',	'COSMIC_Gene~NumSamples', 'MSKCC_CancerGenes',	'Atlas_Oncology',	'Sanger_CancerGenes',	'Sanger_Germline_CancerGenes',	'Sanger_network-informed_CancerGenes~Pval',	'SegDup_Region',	'Gene_Symbol',	'DrugBank',	'Reactome_Pathway',	'Gene_Onotology',	'Disease_Ontology')
+header = ('Gene',	'Gene_Type',	'Location',	'Distance',	'Coding_Impact', 'Protein_Pos',	'Original_AA',	'Allele_AA',	'Start~Stop_Dist',	'Prop_Cons_Affected_Upstream',	'Prop_Cons_Affected_Downstream',	'Trunc_Prediction',	'Conserved46way',	'Conserved46wayPlacental',	'Conserved46wayPrimates',	'ASW_minallele',	'CEU_minallele',	'CHB_minallele',	'CHD_minallele',	'GIH_minallele',	'JPT_minallele',	'LWK_minallele',	'MEX_minallele',	'MKK_minallele',	'TSI_minallele',	'YRI_minallele',	'1000GENOMES_AF',	'WELLDERLY_AF325',	'NHLBI',	'eQTL_genes',	'miRNA_BS_influenced',	'miRNA_BS_impact',	'miRNA_BS_direct',	'miRNA_BS_deltaG',	'miRNA_genomic',	'miRNA_folding_deltaG',	'miRNA_binding_deltaG',	'miRNA_top_targets_changed',	'Splice_Site_Pred',	'Splicing_Prediction(MaxENT)',	'ESE_sites',	'ESS_sites',	'Protein_Impact_Prediction(Polyphen)',	'Protein_Impact_Probability(Polyphen)',	'Protein_Impact_Prediction(SIFT)',	'Protein_Impact_Score(SIFT)',	'Protein_Domains',	'Protein_Domains_Impact(LogRE)',	'Protein_Impact_Prediction(Condel)',	'Protein_Impact_Probability(Condel)',	'TF_Binding_Sites',	'TFBS_deltaS',	'omimGene_ID~omimGene_association',	'Protein_Domain_Gene_Ontology',	'dbSNP_ID',	'HGMD_Variant~PubMedID',	'HGMD_Gene~disease_association',	'Genetic_Association_Database~PubMedID',	'PharmGKB_Database~Drug',	'Inheritance~Penetrance',	'Severity~Treatability',	'COSMIC_Variant~NumSamples',	'COSMIC_Gene~NumSamples', 'MSKCC_CancerGenes',	'Atlas_Oncology',	'Sanger_CancerGenes',	'Sanger_Germline_CancerGenes',	'Sanger_network-informed_CancerGenes~Pval',	'SegDup_Region',	'Gene_Symbol',	'DrugBank',	'Reactome_Pathway',	'Gene_Onotology',	'Disease_Ontology',	'ACMG_Score_Clinical~Disease_Entry~Explanation',	'ACMG_Score_Research~Disease_Entry~Explanation')
 columnNumber = 0
 
 '''
@@ -97,28 +97,20 @@ while line:
         line = invar.readline()
         continue
  	
-    variant_list = PreAnnotationDB.find({'beg':beg, 'end':end,'type':vartype ,'ref':ref,'alt':obs})
+    variant_list = PreAnnotationDB.find_one({'beg':beg, 'end':end,'type':vartype ,'ref':ref,'alt':obs})
    
     if str(variant_list) == "None":
         skiped_var = skiped_var + 1
         if (skiped_var%500 == 0):
            print skiped_var
            print time.asctime( time.localtime(time.time()) )
-        """
-        Insert the variants (for now)
-		"""        
-        annot = ""
-        for int in lin[8:]:
-                annot=annot+int+"@@"
-        
-        
-        post = {'chr':CUR_CHROM,'beg':beg, 'end':end, 'type':vartype, 'ref':ref, 'alt':obs, 'annot':annot}
-        PreAnnotationDB.insert(post)
-         
-		line = invar.readline()
+       
+	line = invar.readline()
         continue	 
     else:
+	annot = ""  
     	for var,val in variant_list.items():
+	   
             '''
             if the variable is not the annotation, coppy to file, else split the annotation at @@ and copy to file
             '''
@@ -127,30 +119,14 @@ while line:
                 '''
                 Change the columnNumber with new value 
                 '''
-                for index, s in enumerate(annot_res):
-			if columnNumber == index:
-                		annot = annot + comment + "@@"
-			else:
-				annot = annot + s + "@@"
-                	#annot_res[columnNumber] = comment
-                	'''
-			Temp, insert '-' in Gene
-			'''
-			#print "Comment is " + str(comment)
-			#annot_res[columnNumber] = comment
-			#if annot_res.index[x] == columnNumber
-                
-                """
+		
+		annot_res[columnNumber] = comment
+		annot = "@@".join(annot_res)
+		'''
                 Insert new value back into the database
                 """	
                 '''
-                for int in annot_res:
-        			annot=annot+"@@"+int
-                '''
-		print "Annotation is " + str(annot)
-        	'''
-        	Variant not present in the preannotation db, update the database with the new variant
-        	'''      
+		print "Annotation is " + str(annot)      
         	post = {'chr':CUR_CHROM,'beg':beg, 'end':end, 'type':vartype, 'ref':ref, 'alt':obs}
 		PreAnnotationDB.update(post, {'chr':CUR_CHROM,'beg':beg, 'end':end, 'type':vartype, 'ref':ref, 'alt':obs, 'annot': annot})
 		if (inserted_variants%1000 == 0):
